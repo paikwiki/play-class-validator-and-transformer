619462b82ad22c539bba3230bec71c652d39bb93
paikwiki <paikwiki@gmail.com>
2023-08-06 23:15:47 +0900

    :construction: 테스트를 위한 임시 코드(WIP)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,28 @@
+import { CatDTO } from "./cat/cat.dto";
+import { Cat } from "./cat/cat.model";
+import { CITY } from "./constants";
+
 const run = () => {
-  console.log("TS Playground");
+  // 🟢 Valid
+  const catDTO1 = new CatDTO("Goni", 13, CITY.London);
+  console.log(catDTO1);
+
+  const cat1 = new Cat("Goni", 13, CITY.London, "abcdefghijabcdefghij", 0, 0);
+  console.log(cat1);
+
+  // ❌ Invalid
+  const catDTO2 = new CatDTO("Goni", -1, CITY.London);
+  console.log(catDTO2);
+
+  const cat2 = new Cat(
+    "Goni",
+    -1,
+    CITY.London,
+    "abcdefghijabcdefghijabcde",
+    0,
+    0,
+  );
+  console.log(cat2);
 };
 
 run();
